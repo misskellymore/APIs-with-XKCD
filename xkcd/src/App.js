5c33cf9b7cb6d3f6c3659b4df1c3dfe77ea544fe
moreira.kd@outlook.com <moreira.kd@outlook.com>
2020-07-21 21:25:20 -0400

    cors anywhere

--- xkcd/src/App.js
+++ xkcd/src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    axios.get('https://cors-anywhere.herokuapp.com/http://xkcd.com/info.0.json')
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+  }, [])
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Testing</h1>
+        <h1>React XKCD</h1>
 
       </header>
     </div>
